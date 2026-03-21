e934e078059cd57efb98ff22989e044b37594811
Monesri <monesri846@gmail.com>
2026-03-21 19:45:24 +0530

    power program

---
+++ Basics/Power.java
@@ -0,0 +1,8 @@
+public class Power {
+    public static void main(String[] args){
+        int x =2, n=3, res=1;
+        for(int i = 1;i<=n;i++) res*=x;
+        System.out.println(res);
+    }
+    
+}
